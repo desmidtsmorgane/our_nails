129d3043177c2d3898102eb430c4863e00fb6af8
desmidtsmorgane <desmidts.morgane@gmail.com>
2015-04-21 16:02:57 +0200

    Create conditions.php

---
+++ conditions.php
@@ -0,0 +1,36 @@
+<?php include('config.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Conditions | Our Nails - Nail Art</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Morgane Desmidts - Web Designer">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1" />
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/styles.css" media="screen" type="text/css" />
+  <link rel="shortcut icon" href="img/favicon.png" />
+</head>
+
+<body>
+
+<?php include('header.php');?>
+
+<div id="container">
+<section id="conditions">
+  <h1>Conditions</h1>
+
+    <p>Informations que vous communiquez : pour accéder à certains services, vous devez créer un compte sur le site. 
+    <p> Dans ce cas, vous fournissez des informations personnelles, telles que votre nom, votre adresse e-mail ou bien un avatar. 
+      Pour pouvoir profiter de toutes les fonctionnalités de partage que le site propose. Sachez qu'aucune de ces données ne sont
+      utilisées à des fins commerciales ou personnelles.</p>
+    <p>Sécurité des données : Nous mettons en œuvre toutes les mesures de sécurité nécessaires pour protéger Ours Nails ainsi que nos utilisateurs 
+      contre tout accès et toute modification, divulgation ou destruction non autorisés des données que nous détenons.
+      Un chiffrement des données (cryptage) est mis en place</p>
+
+</section> <!-- end conditions -->
+</div> <!-- end container -->
+
+<?php include('footer.php');?>
+
+</body>
+</html>
