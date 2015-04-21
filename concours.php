5515ea1173e5385bce8e8409f796abde6f17067a
desmidtsmorgane <desmidts.morgane@gmail.com>
2015-04-21 16:02:32 +0200

    Create concours.php

---
+++ concours.php
@@ -0,0 +1,35 @@
+<?php include('config.php') ?>
+<?php $page = 'concours'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Concours | Our Nails - Nail Art</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Morgane Desmidts - Web Designer">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1" />
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/styles.css" media="screen" type="text/css" />
+  <link rel="shortcut icon" href="img/favicon.png" />
+</head>
+
+<body>
+
+<?php include('header.php');?>
+
+<div id="container">
+<section id="concours">
+  <h1>Concours</h1>
+
+    <p>Participe au concours sur le thème de «l’été», cele qui obtiendra le plus de votes gagnera ce lot:</p>
+    <p>Un gel monophasé, deux gels colorés et 3 limes.</p>
+    <p>Pour ce faire, envoi moi une photo de ta pose avec ton prénom inscrit par-dessus à l’adresse suivante:</p>
+
+    <a href="#" class="button">our-nails@gmail.com</a>
+
+</section> <!-- end concours -->
+</div> <!-- end container -->
+
+<?php include('footer.php');?>
+
+</body>
+</html>
