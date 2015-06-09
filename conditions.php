2479bcc2646122adc2a5c7922a4b84fd3df7b342
desmidtsmorgane <desmidts.morgane@gmail.com>
2015-06-09 11:36:13 +0200

    Update conditions.php

--- conditions.php
+++ conditions.php
@@ -1,36 +1,36 @@
-<?php include('config.php') ?>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Conditions | Our Nails - Nail Art</title>
-  <meta charset="UTF-8">
-  <meta name="description" content="Morgane Desmidts - Web Designer">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1" />
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/styles.css" media="screen" type="text/css" />
-  <link rel="shortcut icon" href="img/favicon.png" />
-</head>
+<?php include('config.php') 
+?><!DOCTYPE html>
+<html lang="fr">
+    <head>
+      <title>Conditions | Our Nails - Nail Art</title>
+      <meta charset="UTF-8">
+      <meta name="description" content="Morgane Desmidts - Web Designer">
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1" />
+      <link rel="stylesheet" href="css/reset.css">
+      <link rel="stylesheet" href="css/styles.css" media="screen" type="text/css" />
+      <link rel="shortcut icon" href="img/favicon.png" />
+    </head>
 
-<body>
+    <body>
 
-<?php include('header.php');?>
+    <?php include('head.php');?>
 
-<div id="container">
-<section id="conditions">
-  <h1>Conditions</h1>
+    <div id="container">
+    <section id="conditions">
+      <h2>Conditions</h2>
 
-    <p>Informations que vous communiquez : pour accéder à certains services, vous devez créer un compte sur le site. 
-    <p> Dans ce cas, vous fournissez des informations personnelles, telles que votre nom, votre adresse e-mail ou bien un avatar. 
-      Pour pouvoir profiter de toutes les fonctionnalités de partage que le site propose. Sachez qu'aucune de ces données ne sont
-      utilisées à des fins commerciales ou personnelles.</p>
-    <p>Sécurité des données : Nous mettons en œuvre toutes les mesures de sécurité nécessaires pour protéger Ours Nails ainsi que nos utilisateurs 
-      contre tout accès et toute modification, divulgation ou destruction non autorisés des données que nous détenons.
-      Un chiffrement des données (cryptage) est mis en place</p>
+        <p>Informations que vous communiquez : pour accéder à certains services, vous devez créer un compte sur le site. 
+        <p> Dans ce cas, vous fournissez des informations personnelles, telles que votre nom, votre adresse e-mail ou bien un avatar. 
+          Pour pouvoir profiter de toutes les fonctionnalités de partage que le site propose. Sachez qu'aucune de ces données n'est utilisée
+          utilisées à des fins commerciales ou personnelles.</p>
+        <p>Sécurité des données : Nous mettons en œuvre toutes les mesures de sécurité nécessaires pour protéger Ours Nails ainsi que nos utilisateurs 
+          contre tout accès et toute modification, divulgation ou destruction non autorisée des données que nous détenons.
+          Un chiffrement des données (cryptage) est mis en place.</p>
 
-</section> <!-- end conditions -->
-</div> <!-- end container -->
+    </section> <!-- end conditions -->
+    </div> <!-- end container -->
 
-<?php include('footer.php');?>
+    <?php include('footer.php');?>
 
-</body>
+    </body>
 </html>
