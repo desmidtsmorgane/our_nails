2a221c38579e4309421255049e4031ee28935023
desmidtsmorgane <desmidts.morgane@gmail.com>
2015-06-09 11:39:33 +0200

    Create credits.php

---
+++ credits.php
@@ -0,0 +1,38 @@
+<?php include('config.php') 
+?><!DOCTYPE html>
+<html lang="fr">
+    <head>
+      <title>Crédits | Our Nails - Nail Art</title>
+      <meta charset="UTF-8">
+      <meta name="description" content="Morgane Desmidts - Web Designer">
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1" />
+      <link rel="stylesheet" href="css/reset.css">
+      <link rel="stylesheet" href="css/styles.css" media="screen" type="text/css" />
+      <link rel="shortcut icon" href="img/favicon.png" />
+    </head>
+
+    <body>
+
+    <?php include('head.php');?>
+
+    <div id="container">
+    <section id="credits">
+      <h2>Crédits</h2>
+
+      <p>Je remercie mes professeurs de DWM pour leurs précieux conseils et avis ainsi que mes caramades Adrien, Julien et Cindy.</p>
+
+      <h3>Mes sources</h3>
+      <ul>
+          <li><a href="#">Menu hamburger</a></li>
+          <li><a href="#">PDO</a></li>
+          <li><a href="#">jQuery</a></li>
+          <li></li>
+      </ul>
+
+    </section> <!-- end credits -->
+    </div> <!-- end container -->
+
+    <?php include('footer.php');?>
+
+    </body>
+</html>
